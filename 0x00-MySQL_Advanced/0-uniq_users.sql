fd64439c0e9b6f64ec33b7ba6762290f7ec44c83
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-14 05:45:04 +0300

    Update 0-uniq_users.sql

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,9 @@
+-- creates a table users
+
+-- attributes id, email, name
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
