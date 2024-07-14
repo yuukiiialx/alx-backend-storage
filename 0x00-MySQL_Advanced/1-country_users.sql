865a5b01d10497c79fbfa070dc8c73ca8fa7940e
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-14 05:45:17 +0300

    Update 1-country_users.sql

---
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -0,0 +1,10 @@
+-- creates a table users
+
+-- attributes id, email, name, country
+
+CREATE TABLE IF NOT EXISTS users(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255),
+    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US'
+);
