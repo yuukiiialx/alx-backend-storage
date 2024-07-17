fa23008daace169c804326700d775928c0ec7ced
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-17 05:47:17 +0300

    Update 11-schools_by_topic.py

--- 0x01-NoSQL/11-schools_by_topic.py
+++ 0x01-NoSQL/11-schools_by_topic.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+    11-schools_by_topic
+"""
+
+
+def schools_by_topic(mongo_collection, topic):
+    """returns the list of school having a specific topic"""
+
+    return mongo_collection.find({"topics": topic})
