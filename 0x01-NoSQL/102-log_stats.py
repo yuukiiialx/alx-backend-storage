2bdb8ef24a467abcd486d2a79d0ede0a29e82af3
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-17 05:47:01 +0300

    Update 102-log_stats.py

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -1,1 +1,35 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+""" provides some stats about Nginx logs stored in MongoDB """
+from pymongo import MongoClient
+
+
+def main():
+    """provides some stats about Nginx logs stored in MongoDB"""
+    client = MongoClient("mongodb://127.0.0.1:27017")
+    nginx_c = client.logs.nginx
+
+    n_logs = nginx_c.count_documents({})
+    print(f"{n_logs} logs")
+    methods: list[str] = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+    for m in methods:
+        c: int = nginx_c.count_documents({"method": m})
+        print(f"\tmethod {m}: {c}")
+
+    st_c: int = nginx_c.count_documents({"method": "GET", "path": "/status"})
+    print(f"{st_c} status check")
+    print("IPs:")
+    top_ips = nginx_c.aggregate(
+        [
+            {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+            {"$sort": {"count": -1}},
+            {"$limit": 10},
+        ]
+    )
+
+    for ip in top_ips:
+        print(f"\t{ip.get('_id')}: {ip.get('count')}")
+        
+
+if __name__ == "__main__":
+    main()
