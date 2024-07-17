cf7a036b4b6b2682c84f7fe2933d241f25800068
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-17 05:46:27 +0300

    Update 10-update_topics.py

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -1,1 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+    10-update_topics.py
+"""
+
+
+def update_topics(mongo_collection, name, topics):
+    """changes all topics of a school document based on the name"""
+
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
