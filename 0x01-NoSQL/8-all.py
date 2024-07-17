26ebd9a83b38f7cee43030e9355d3681064c07a1
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-17 05:49:17 +0300

    Update 8-all.py

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,1 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""
+    8-all.py
+"""
+
+
+def list_all(mongo_collection) -> list:
+    """list all documents in a collection"""
+    return mongo_collection.find()
