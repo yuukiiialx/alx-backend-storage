d6fbc30f0da6120db84891a269b172484c506dd9
Mohamed Emad Eldin <140041229+yuukiiialx@users.noreply.github.com>
2024-07-17 05:46:47 +0300

    Update 101-students.py

--- 0x01-NoSQL/101-students.py
+++ 0x01-NoSQL/101-students.py
@@ -1,1 +1,13 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+"""Module for 101-students"""
+
+
+def top_students(mongo_collection):
+    """returns all students sorted by average score"""
+    return mongo_collection.aggregate([
+        {"$project": {
+            "name": "$name",
+            "averageScore": {"$avg": "$topics.score"}
+        }},
+        {"$sort": {"averageScore": -1}}
+    ])
